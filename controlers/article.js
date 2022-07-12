e2de9c465163e8b32684ab9f789cdec74a042853
ss-Muss <correoadfr@gmail.com>
2022-07-11 23:56:15 -0600

    reparando editar articulo

--- controlers/article.js
+++ controlers/article.js
@@ -219,7 +219,7 @@ var controller = {
                         message: 'No existe el articulo'
                     });
                 }
-                return res.status(404).send({
+                return res.status(200).send({
                     status : 'success',
                     article: articleUpdated 
                 });
